2934fa7e02a3b8ec4efb4f39e742c75bff50cb29
lukemew <lucasmelor65@gmail.com>
2023-02-26 12:09:07 -0300

    terminando o exercicio

---
+++ módulo-14.js
@@ -0,0 +1,26 @@
+var branco = preto
+var preto = cinza
+var cinza = branco
+var carro = preto
+var cavalo = carro
+var valor = 30000
+var prestacao = 750
+
+var a = (branco == 'branco')
+console.log(a)
+
+var b = (branco == cinza)
+console.log(b)
+
+var c = (carro === branco)
+console.log(c)
+
+var d = (cavalo = carro == "preto" ? "cinza" : "marron")
+console.log(d)
+
+var e = (valor - 3000) / prestacao
+console.log(e)
+
+var f = ("branco" + "preto" + "cinza")
+console.log (f + ", é formada uma string de " + f.length + " caracteres.")
+
